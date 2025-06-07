efd92b0dd8572beaf2c380d1ed056feb02e0dd95
Martobgz <118079877+Martobgz@users.noreply.github.com>
2025-06-07 14:15:35 +0300

    Replay
    pseudo code

---
+++ replay.c
@@ -0,0 +1,28 @@
+#include "Btlshp.h"
+void playReplay(filename){
+//Board_State P1 = // readvam b1 maham gi
+  //  Board_State P2 = // readvam b2
+
+    Board_State boards[2];
+
+//    boards[0] = // readvam b1
+   // boards[1] = //readvam b2
+    // display bordovete bez hodove polvzam display player board
+    while(1){
+        //if(endoffile) break;
+        
+        //print bordove s hodove polzvam opponent board
+        // cheta target
+        // cheta c.x i c.y
+        Coords c;
+        c.x = // cheta
+        c.y = //cheta 
+        fire(boards[target], c);
+
+
+
+
+
+
+    }
+}
